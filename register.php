e91db5b30a6ed8e26ca775cee6d3646ecc2fbab7
elkassir hamza <elkassir.hamza05@gmail.com>
2020-05-25 00:39:47 +0000

    forme register

---
+++ register.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>The Perfect Cup - Blog</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/business-casual.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+    <!-- Fonts -->
+    <link
+        href="http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800"
+        rel="stylesheet" type="text/css">
+    <link
+        href="http://fonts.googleapis.com/css?family=Josefin+Slab:100,300,400,600,700,100italic,300italic,400italic,600italic,700italic"
+        rel="stylesheet" type="text/css">
+</head>
+
+<body>
+    <div class="form-container">
+
+
+
+        <form role="form">
+            <h2 class="title">Registration form </h2>
+            <div class="row regis ">
+                <div class="form-group col-lg-6">
+                    <label>Name</label>
+                    <input type="text" id="fname" name="fname" maxlength="25" class="form-control">
+                </div>
+               
+                <div class="clearfix"></div>
+               
+                <div class="form-group col-lg-6">
+                    <label>Email Address</label>
+                    <input type="email" id="email" name="email" maxlength="25" class="form-control">
+                </div>
+                
+                <div class="form-group col-lg-6">
+                    <label>password</label>
+                    <input type="password" id="password" name="password" maxlength="25" class="form-control">
+                </div>
+                <div class="form-group col-lg-12">
+                    <label>ADRESS</label>
+                    <textarea class="form-control" id="ADRESS" name="ADRESS" maxlength="100" rows="1"></textarea>
+                </div>
+                <div class="form-group col-lg-12">
+                    <button type="submit" id="contact" class="btn btn-primary">Submit</button>
+                </div>
+            </div>
+
+        </form>
+
+    </div>
+</body>
+
+</html>
